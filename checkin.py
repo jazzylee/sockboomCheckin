1b48b92d2c6d56fe7917a8e45a4c4a95ebf4cb5a
“jazzylee” <1902638751@qq.com>
2022-04-07 15:12:56 +0800

    version2:email informing

--- checkin.py
+++ checkin.py
@@ -1,29 +1,69 @@
-import os
 import json
 import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
-r = requests.session()
-loginData = {"email":"","passwd":""}
-loginRes = r.post("https://sockboom.lol/auth/login",data=loginData)
 
-try:
-    loginResJson = json.loads(loginRes.text)
-    if loginResJson['ret'] == 1:
-        print("success login")
-except:
-    print("fail login")
-    raise
+loginUrl = "https://sockboom.lol/auth/login"
+checkinUrl = "https://sockboom.lol/user/checkin"
+lououtUrl = "https://sockboom.lol/user/logout"
+loginData = {"email":"","passwd":""}#需要填写sockboom登录信息
+my_sender=''#邮件发送者
+my_pass = '' #授权码
+my_user='' #邮件接收者(可以和发送者相同)
 
-checkinRes = r.post('https://sockboom.lol/user/checkin')
+def checkin():
+    r = requests.session()
+    try:
+        loginRes = r.post(loginUrl,data=loginData)
+        loginResJson = json.loads(loginRes.text)
+        if loginResJson['ret'] == 1:
+            print("success login")
+    except:
+        print("fail login")
+        return False
 
-try:
-    checkinResJson = json.loads(checkinRes.text)
-    assert checkinResJson['ret'] == 1
-    print("success checkin")
-    print(checkinResJson['msg'])
-except:
-    print("fail checkin")
-    raise
+    try:
+        checkinRes = r.post(checkinUrl)
+        checkinResJson = json.loads(checkinRes.text)
+        assert checkinResJson['ret'] == 1
+        print("success checkin")
+        print(checkinResJson['msg'])
+    except:
+        print("fail checkin")
+        return False
 
-logout = r.get("https://sockboom.lol/user/logout")
-os.system ("pause") 
+    logout = r.get(lououtUrl)
+    return checkinResJson['msg']
+
+def mail(msg):
+    try:
+        msg=MIMEText(msg,'plain','utf-8')
+        msg['From']=formataddr(["FromRunoob",my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To']=formataddr(["FK",my_user])              # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject']="Sockboom签到状态提醒"                # 邮件的主题，也可以说是标题
+ 
+        server=smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(my_sender,[my_user,],msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()
+        return True
+    except Exception:
+        print("邮件发送失败")
+        return False
+ 
+if __name__=="__main__":
+    checkinRes = checkin()
+    if checkinRes:
+        ret=mail(checkinRes)
+        if ret:
+            print("签到成功,邮件发送成功")
+        else:
+            print("签到成功，邮件发送失败")
+    else:
+        ret=mail("签到失败")
+        if ret:
+            print("签到失败,邮件发送成功")
+        else:
+            print("签到失败，邮件发送失败")
